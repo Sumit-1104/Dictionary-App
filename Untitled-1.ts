946e0d76b33c01d5f50420961b8721cca9c2dd9a
Sumit Satpute <satputesumit005@gmail.com>
2025-04-28 11:12:55 +0530

    Add files via upload

---
+++ Untitled-1.ts
@@ -0,0 +1,16 @@
+// try {
+//   const response = await fetch("https://twinword-word-graph-dictionary.p.rapidapi.com/theme/?entry=ridiculous", {
+//     method: 'GET',
+//     headers: {
+//       'X-RapidAPI-Key': '302b9ce08bmsh96884dbcc4f35dfp18adfajsnb1c92c68af39',
+//       'X-RapidAPI-Host': 'twinword-word-graph-dictionary.p.rapidapi.com'
+//     }
+//   });
+
+//   if (response.ok) {
+//     const result = await response.json();
+//     console.log(result);
+//   }
+// } catch (err) {
+//   console.error(err);
+// }
